6ed732cbeafc4f8f72ecee16775c34b2f525c48b
Adrien Narkiewicz <adrien.nkwz@gmail.com>
2022-06-01 17:13:29 +0200

    ForumFeature

---
+++ src/entities/Topic.java
@@ -0,0 +1,72 @@
+package entities;
+
+import java.util.Collection;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Topic {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String auteur;
+	private String titre;
+	private String description;
+	
+	@OneToMany(mappedBy="topic",fetch = FetchType.EAGER)
+	private Collection<Message> messages;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Collection<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(Collection<Message> messages) {
+		this.messages = messages;
+	}
+	
+	public void addMessages(Collection message) {
+		this.messages.addAll(message);
+	}
+	
+
+}
